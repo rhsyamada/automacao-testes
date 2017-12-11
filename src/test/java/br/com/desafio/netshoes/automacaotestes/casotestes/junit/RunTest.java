8925af3f4db2fb868cc2e9790cd7dac7fbf36744
Ricardo Yamada <hikariya@inmetrics.com.br>
2017-12-11 17:57:37 -0200

    Alteração no package da runtest

--- src/test/java/br/com/desafio/netshoes/automacaotestes/casotestes/junit/RunTest.java
+++ src/test/java/br/com/desafio/netshoes/automacaotestes/casotestes/junit/RunTest.java
@@ -8,6 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, snippets = SnippetType.CAMELCASE, plugin = {
 		"json:target/surefire-reports/cucumber.json" }, features = { ".\\src\\test\\resources\\" }, glue = {
-				"br.com.desafio.netshoes.automacaotestes.automation.step" }, tags= {"@netshoesArgentina"})
+				"br.com.desafio.netshoes.automacaotestes.automation.step",
+				"br.com.desafio.netshoes.automacaotestes.configuration" })
 public class RunTest {
 }
